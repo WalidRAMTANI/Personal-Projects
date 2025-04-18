4ec8aaa78b1bf55d1a4db13ec19fc78ad140c0f1
WalidRAMTANI <walid.ramtani@yahoo.com>
2025-04-18 23:59:27 +0200

    Delete python/breakout_game directory

--- python/breakout_game/breakout_game/brique.py
+++
@@ -1,9 +0,0 @@
-class brique:
-    def __init__(self, x_g, y_g, x_d, y_d, bonus, color):
-        self.x_g = x_g
-        self.y_g = y_g
-        self.x_d = x_d
-        self.y_d = y_d
-        self.taille = x_d - x_g
-        self.bonus = bonus
-        self.color = color
